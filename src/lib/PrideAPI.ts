baa39564e3c67ffae2e43d30ca00abe7a97c8de6
Gheric Speiginer <gheric.speiginer@gmail.com>
2020-01-28 15:15:08 -0500

    fix: Inherit protocol for PRIDE API request from the current page

--- src/lib/PrideAPI.ts
+++ src/lib/PrideAPI.ts
@@ -3,7 +3,7 @@ const MOCK_PROCEDURES = {
 }
 
 // const BASE_URL = 'http://defiant3.local:8025/AR_server/'
-const BASE_URL = 'http://prideview-ar.traclabs.com:8025/AR_server/'
+const BASE_URL = '//prideview-ar.traclabs.com:8025/AR_server/'
 
 
 class InstructionType {
